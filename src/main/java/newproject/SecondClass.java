c3d02ebe2bdcba9648622faeec85249a9bd9da8b
durmusmustafa <techpromustafa@gmail.com>
2023-03-13 20:24:23 +0100

    new second snd third

---
+++ src/main/java/newproject/SecondClass.java
@@ -0,0 +1,7 @@
+package newproject;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println(" second class");
+    }
+}
---
+++ src/main/java/newproject/ThirdClass.java
@@ -0,0 +1,7 @@
+package newproject;
+
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("third");
+    }
+}
